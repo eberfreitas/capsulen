c83616ced6c9f107e1b5f4e8a26b55adcadeb142
Éber Freitas Dias <eber.freitas@gmail.com>
2023-09-11 23:14:50 -0300

    Old stuff is gone...

--- _old/public/src/List/Extra.elm
+++
@@ -1,9 +0,0 @@
-module List.Extra exposing (indexedFilter)
-
-
-indexedFilter : (Int -> a -> Bool) -> List a -> List a
-indexedFilter f list =
-    list
-        |> List.indexedMap Tuple.pair
-        |> List.filter (\( i, item ) -> f i item)
-        |> List.map Tuple.second
