eb3c9fbf914e9ae754a2f746b43aa4d413d4ca23
Éber Freitas Dias <eber.freitas@gmail.com>
2023-12-13 23:33:21 -0300

    Html style with elm-css (magic)

--- client/src/View/Logo.elm
+++ client/src/View/Logo.elm
@@ -18,12 +18,12 @@ logo size color =
             [ SvgAttributes.cx "87.5"
             , SvgAttributes.cy "65.5"
             , SvgAttributes.r "22.5"
-            , SvgAttributes.fill <| Color.toCss color
+            , SvgAttributes.fill <| Color.toString color
             ]
             []
         , Svg.path
             [ SvgAttributes.d "M65 0c35.874 0 65 29.126 65 65 0 35.874-29.126 65-65 65-35.874 0-65-29.126-65-65C0 29.126 29.126 0 65 0Zm0 10c-30.355 0-55 24.645-55 55s24.645 55 55 55 55-24.645 55-55-24.645-55-55-55Z"
-            , SvgAttributes.fill <| Color.toCss color
+            , SvgAttributes.fill <| Color.toString color
             ]
             []
         ]
