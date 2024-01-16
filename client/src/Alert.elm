9afadac1c089a269598266d089cfb40624e23bf6
Éber Freitas Dias <eber.freitas@gmail.com>
2024-01-16 02:35:11 -0300

    Adjusts alert styling

--- client/src/Alert.elm
+++ client/src/Alert.elm
@@ -1,12 +1,12 @@
 module Alert exposing (Message, Severity(..), new, toHtml)
 
+import Css
 import Html.Styled as Html
 import Html.Styled.Attributes as HtmlAttributes
 import Html.Styled.Events as HtmlEvents
 import Phosphor
-import Css
-import View.Theme
 import View.Color
+import View.Theme
 
 
 type Severity
@@ -41,22 +41,32 @@ toHtml : View.Theme.Theme -> (Int -> msg) -> Int -> Message -> Html.Html msg
 toHtml theme closeFn index (Message message) =
     Html.div
         [ HtmlAttributes.css
-            [ Css.bottom <| Css.px 0
-            , Css.left <| Css.pct 50
-            , Css.maxWidth <| Css.px 700
-            , Css.position Css.fixed
-            , Css.transform <| Css.translate (Css.pct -50)
-            , Css.width <| Css.pct 100
-            , Css.margin2 (Css.px 0) (Css.auto)
-            , Css.backgroundColor (theme |> View.Theme.foregroundColor |> View.Color.toCss)
+            [ Css.backgroundColor (theme |> View.Theme.foregroundColor |> View.Color.toCss)
             , Css.color (theme |> View.Theme.backgroundColor |> View.Color.toCss)
             , Css.fontWeight Css.bold
             , Css.padding <| Css.rem 1
+            , Css.marginBottom <| Css.rem 1
+            , Css.borderRadius <| Css.rem 1
+            , Css.border3 (Css.rem 0.5) Css.solid (theme |> View.Theme.backgroundColor |> View.Color.toCss)
+            , Css.position Css.relative
             ]
         ]
         [ Html.div [] [ Html.text <| message.body ]
         , Html.div []
-            [ Html.button [ HtmlEvents.onClick <| closeFn index ]
+            [ Html.button
+                [ HtmlEvents.onClick <| closeFn index
+                , HtmlAttributes.css
+                    [ Css.position Css.absolute
+                    , Css.top <| Css.px 0
+                    , Css.right <| Css.px 0
+                    , Css.backgroundColor Css.transparent
+                    , Css.border <| Css.px 0
+                    , Css.fontSize <| Css.rem 2
+                    , Css.margin <| Css.px 0
+                    , Css.padding <| Css.rem 0.5
+                    , Css.cursor Css.pointer
+                    ]
+                ]
                 [ Phosphor.xCircle Phosphor.Regular |> Phosphor.toHtml [] |> Html.fromUnstyled ]
             ]
         ]
--- client/src/View/Alerts.elm
+++ client/src/View/Alerts.elm
@@ -1,8 +1,10 @@
 module View.Alerts exposing (Msg, update, view)
 
 import Alert
+import Css
 import Effect
 import Html.Styled as Html
+import Html.Styled.Attributes as HtmlAttributes
 import View.Theme
 
 
@@ -12,7 +14,18 @@ type Msg
 
 view : View.Theme.Theme -> List Alert.Message -> Html.Html Msg
 view theme alerts =
-    Html.div [] (alerts |> List.indexedMap (\index alert -> Alert.toHtml theme CloseAlert index alert))
+    Html.div
+        [ HtmlAttributes.css
+            [ Css.bottom <| Css.px 0
+            , Css.left <| Css.pct 50
+            , Css.maxWidth <| Css.px 700
+            , Css.position Css.fixed
+            , Css.transform <| Css.translate (Css.pct -50)
+            , Css.width <| Css.pct 100
+            , Css.margin2 (Css.px 0) Css.auto
+            ]
+        ]
+        (alerts |> List.indexedMap (\index alert -> Alert.toHtml theme CloseAlert index alert))
 
 
 update : Msg -> Effect.Effect
